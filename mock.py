87f87bd5a9ce32dfe32cc546319216279c9455d1
Patrick Pang <imptrik@gmail.com>
2020-05-06 13:35:40 +0800

    fix z-index & add max-width

--- mock.py
+++ mock.py
@@ -15,13 +15,19 @@ def home():
 @app.post("/detect")
 async def detect(file: UploadFile = File(...)):
     await sleep(5)  # simulate inference
-    # landmarks = []
-    # landmarks = [[115.0, 122.0, 179.0, 121.0, 133.0, 169.0]]
-    # landmarks = [[263.0, 433.0, 379.0, 286.0, 418.0, 466.0]]
-    landmarks = [
-        [408, 694, 507, 681, 458, 749],
-        [750, 436, 884, 437, 809, 543],
-        [1050, 685, 1152, 691, 1084, 757],
-        [1359, 595, 1453, 611, 1380, 666],
-    ]
+
+    if file.filename == "test1.jpg":
+        landmarks = [[115.0, 122.0, 179.0, 121.0, 133.0, 169.0]]
+    elif file.filename == "test2.jpg":
+        landmarks = [[263.0, 433.0, 379.0, 286.0, 418.0, 466.0]]
+    elif file.filename == "test3.jpg":
+        landmarks = [
+            [408, 694, 507, 681, 458, 749],
+            [750, 436, 884, 437, 809, 543],
+            [1050, 685, 1152, 691, 1084, 757],
+            [1359, 595, 1453, 611, 1380, 666],
+        ]
+    else:
+        landmarks = []
+
     return {"success": True, "landmarks": landmarks}
