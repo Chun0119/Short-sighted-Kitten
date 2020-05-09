666cbc73fac1798400c582dc0493c9deccc39b47
Patrick Pang <imptrik@gmail.com>
2020-05-09 19:02:00 +0800

    add colab server option

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Load models
 tf.get_logger().setLevel("ERROR")
 print("Loading Models...")
 face_detection_model = tf.keras.models.load_model(
@@ -23,10 +24,19 @@ def home():
 
 
 def detect_face(img):
+    """Detect cat faces in an image 
+
+    Args:
+        img: raw bytes of .jpg, .png, .gif, .bmp
+    Returns:
+        a list of zero or more bounding boxes, i.e. [[x0, y0, x1, y1]]
+    """
+
+    # decode image
     img = tf.io.decode_image(img, channels=3, expand_animations=False)
-    shape = tf.shape(img)[:2]  # [height, width]
     img = tf.cast(img, tf.uint8)
 
+    # predict using face detection model
     inputs = tf.expand_dims(img, 0)
     outputs = face_detection_model(inputs)
     results = outputs["detections:0"][0]
@@ -36,10 +46,21 @@ def detect_face(img):
         for bbox in bboxes
     ]
 
-    return img, bboxes, shape
+    return img, bboxes
 
 
-def detect_landmarks(img, shape, bbox):
+def detect_landmarks(img, bbox):
+    """Detect cat facial landmarks in face ROI 
+
+    Args:
+        img: decoded image
+        bbox: bounding box
+    Returns:
+        a list of landmarks, i.e. [left eye x, left eye y, right eye x, right eye y, mouth x, mouth y]
+    """
+
+    # crop ROI
+    shape = tf.shape(img)[:2]  # [height, width]
     dimensions = tf.reverse(shape, axis=[-1])
     dimensions = tf.cast(dimensions, tf.float32)
     normalized = tf.reshape(bbox, [-1, 2]) / dimensions
@@ -52,6 +73,7 @@ def detect_landmarks(img, shape, bbox):
     )[0]
     roi = (roi / 127.5) - 1  # rescaling to [-1, 1]
 
+    # predict using facial landmarks model
     inputs = tf.expand_dims(roi, 0)
     outputs = landmarks_model.predict(inputs)
     landmarks = outputs[0]
@@ -59,7 +81,16 @@ def detect_landmarks(img, shape, bbox):
     return landmarks
 
 
-def project_landmarks(shape, bbox, landmarks):
+def project_landmarks(bbox, landmarks):
+    """Project landmarks on resized ROI to original image  
+
+    Args:
+        bbox: bounding box
+        landmarks: a list of landmarks
+    Returns:
+        a list of projected landmarks
+    """
+
     # project landmarks from IMG_SIZE_LANDMARKS to bbox
     width = bbox[2] - bbox[0]
     height = bbox[3] - bbox[1]
@@ -73,15 +104,24 @@ def project_landmarks(shape, bbox, landmarks):
     landmarks = tf.reshape(landmarks, [-1])
     return landmarks
 
+
 def detect_cat(raw):
-    img, bboxes, shape = detect_face(raw)
-    landmarks = [detect_landmarks(img, shape, bbox) for bbox in bboxes]
+    """Detect cat facial landmarks in an image 
+
+    Args:
+        raw: raw bytes of .jpg, .png, .gif, .bmp
+    Returns:
+        a list of landmarks on cat faces, i.e. [[left eye x, left eye y, right eye x, right eye y, mouth x, mouth y]]
+    """
+
+    img, bboxes = detect_face(raw)
+    landmarks = [detect_landmarks(img, bbox) for bbox in bboxes]
     landmarks = [
-        project_landmarks(shape, bbox, landmark)
-        for landmark, bbox in zip(landmarks, bboxes)
+        project_landmarks(bbox, landmark) for landmark, bbox in zip(landmarks, bboxes)
     ]
-    landmarks = [landmark.numpy().tolist() for landmark in landmarks]
+    landmarks = [landmark.numpy().tolist() for landmark in landmarks] # convert tf.Tensor into Python list
     return landmarks
+
 
 @app.post("/detect")
 def detect(file: UploadFile = File(...)):
